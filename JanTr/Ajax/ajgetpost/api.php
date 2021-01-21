b500370a14dad1378c1e3a18e316815392c63373
leonachen <waterfay0.0@gmail.com>
2021-01-21 16:09:13 +0800

    ajax

--- JanTr/Ajax/ajgetpost/api.php
+++ JanTr/Ajax/ajgetpost/api.php
@@ -16,8 +16,8 @@ switch($_GET['todo']){
                 <td>'.$row['info'].'</td>
                 <td>'.$row['date'].'</td>
                 <td>
-                    <button>修改</button>
-                    <button>刪除</button>
+                    <button class="mdy">修改</button>
+                    <button onclick="del(this);">刪除</button>
                 </td>
                 </tr>
                 ';
@@ -26,7 +26,25 @@ switch($_GET['todo']){
             echo 'fail';
         }
         break;
-}
+    case 'update':
+        // print_r($_POST);
+        $sql="UPDATE `ajax_animal` SET name='" . $_POST['name'] . "',weight='" . $_POST['weight'] . "',info='" . $_POST['info'] . "',date=NOW() WHERE id='" . $_POST['id'] . "'";
+        // UPDATE `ajax_animal` SET name='藪貓',weight='9',info='食肉目 貓科 藪貓屬',date=NOW() WHERE id='1'
+        $result= $db->query($sql);
+        if($result) echo date("Y-m-d H:i:s");
+        break;
+    case 'delete':
+        $sql="DELETE from ajax_animal WHERE id=".$_POST['id'];
+        $result = $db->query($sql);
+        if($result) echo 'delete';
+        break;
+    case 'insert':
+            "INSERT INTO ajax_animal VALUES(null,'".$_POST['name']."',".$_POST['weight'].",'".$_POST['info'].")";
+            $result=$db->query($sql);
+            if($result) echo 'inserted';
+        break;
+
+    }
 
 
 ?>
